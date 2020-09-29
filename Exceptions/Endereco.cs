94302b847c1fc00664b6b520c51fafe7d5ecd09f
greensystem2000 <greensystem2000@gmail.com>
2020-09-29 20:12:20 -0300

    Exceptions

---
+++ Exceptions/Endereco.cs
@@ -0,0 +1,22 @@
+﻿namespace Collections
+{
+    class Endereco : IEndereco
+    {
+        public string TipoEndereco { get; set; }
+        public long Id { get; set; }
+        public string Cidade{ get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
+
+        public string Consultar()
+        {
+            return $"Tipo endereco {TipoEndereco}\n" +
+                   $"ID: {Id}\n" +
+                   $"Cidade: {Cidade}\n" +
+                   $"Logradouro: {Logradouro}\n" +
+                   $"Bairro: {Bairro}\n" +
+                   $"Complemento: {Complemento}";
+        }
+    }
+}
---
+++ Exceptions/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collections
+{
+    abstract class Pessoa
+    {
+        public string nome { get; set; }
+        public DateTime dtNascFund { get; set; }
+        public string email { get; set; }
+        public Endereco endereco;
+    }
+}
